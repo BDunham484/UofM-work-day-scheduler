b30527e4e3e1daaa5912f436812105eea86bcaab
ThereWolf <bdunham484@gmail.com>
2023-04-06 15:24:05 -0500

    challenge work

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,66 @@
 let currentDayEl = $('#currentDay');
+let containerEl = $('#container');
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    let hour;
+    let militaryHour;
+    for (i = 0; i < 9; i++) {
+        if ((i + 9) < 12) {
+            hour = i + 9
+            amPm = 'AM'
+            militaryHour = i + 9;
+        } else if ((i + 9) === 12) {
+            hour = 12
+            amPm = 'PM'
+            militaryHour = i + 9;
+        } else {
+            hour = i - 3
+            amPm = 'PM'
+            militaryHour = i + 9;
+        }
+        
+        let timeBlockRow = $('<div>')
+            .attr('id', 'hour-' + hour)
+            .attr('class', 'row time-block');
+
+        let timeOfDay = $('<div>')
+            .attr('class', 'col-2 col-md-1 hour text-center')
+            .text(hour + amPm);
+
+        let textArea = $('<textarea>')
+            .attr('class', 'col-8 col-md-10 description')
+            .attr('rows', '3');
+
+        let saveButton = $('<button>')
+            .attr('class', 'btn saveBtn col-2 col-md-1')
+            .attr('aria-label', 'save');
+
+        let icon = $('<i>')
+            .attr('class', 'fas fa-save')
+            .attr('aria-hidden', 'true');
+
+
+        timeBlockRow.append(timeOfDay);
+        timeBlockRow.append(textArea);
+        timeBlockRow.append(saveButton);
+        saveButton.append(icon);
+        containerEl.append(timeBlockRow);
+
+
+        let currentHour = dayjs().hour()
+
+        if (currentHour > militaryHour) {
+            textArea.addClass('past')
+        } else if (currentHour === militaryHour) {
+            textArea.addClass('present');
+        } else {
+            textArea.addClass('future');
+        }
+
+    }
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -22,10 +79,11 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-    let hour = ((1000 * 60) * 60)
-    const getToday = () => {
-        let today = dayjs();
-        currentDayEl.text(today.format('MMM D, YYYY'))
-    }
-    setInterval(getToday, hour);
+    currentDayEl.text(dayjs().format('MMM D, YYYY'))
+
+    // let interval = ((1000 * 60) * 60)
+    // const getToday = () => {
+    //     currentDayEl.text(dayjs().format('MMM D, YYYY'))
+    // }
+    // setInterval(getToday, interval);
 });
